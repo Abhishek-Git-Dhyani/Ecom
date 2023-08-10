39b49b92970f31e706cceb16c080d196b3991c8f
Abhishek Abhishek <16891@GENUSDOMAIN.ORG>
2023-08-10 20:08:52 +0530

    Adding ironPDF for dynamic PDF creation

---
+++ Ecom/Ecom/Controllers/ReportsController.cs
@@ -0,0 +1,37 @@
+﻿using Ecom.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IronPdf;
+
+namespace Ecom.Controllers
+{
+    public class ReportsController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetReport(string userId)
+        {
+            try
+            {
+                using(DbEntities db = new DbEntities())
+                {
+                    var user = db.userReports.FirstOrDefault(x=>x.userId == userId).CBC_Report;
+
+                    var Renderer = new HtmlToPdf();
+
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            //var Renderer = new HtmlToPdf();
+            ////Renderer = Renderer.
+            //return View();
+        }
+    }
+}
